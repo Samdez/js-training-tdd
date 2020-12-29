bf1b6747541f0c1f43447ef6e690e6dbdf72c0f7
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-29 10:01:19 +0100

    solved same-sign

--- exercises/same-sign.js
+++ exercises/same-sign.js
@@ -8,6 +8,20 @@
  */
 
 // Your code:
+const sign = (num) => {
+  if (num > 0) 
+    return 1
+  else if (num < 0)
+    return -1
+  else 
+    return 0
+}
+
+const sameSign = (a, b) => {
+  const signA = sign(a)
+  const signB = sign(b)
+  return signA === signB 
+}
 
 //* Begin of tests
 const assert = require('assert');
