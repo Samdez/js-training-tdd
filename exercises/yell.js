b2d9e937b14ff639b3b6514fad365eff7ce4a064
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-29 10:49:08 +0100

    solved yell

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,15 @@
  */
 
 // Your code:
+const yell = (str) => str.split('').map(letter => letter.toUpperCase()).join('')
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function')
+assert.strictEqual(yell.length, 1)
+assert.strictEqual(yell('hello'), 'HELLO')
+assert.strictEqual(yell('hello world'), 'HELLO WORLD')
+
+// assert.fail('You must write your own tests');
 // End of tests */
