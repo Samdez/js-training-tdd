503e72f7241b28380d9137439bc32923b5592470
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-29 10:16:37 +0100

    solved total

--- exercises/total.js
+++ exercises/total.js
@@ -7,13 +7,16 @@
  */
 
 // Your code:
-
+const total = (arr) => {
+  const reducer = (acc, val) => acc + val
+  return arr.reduce(reducer, 0)
+} 
 //* Begin of tests
 const assert = require('assert');
 
 assert.strictEqual(typeof total, 'function');
 assert.strictEqual(total.length, 1);
-assert.deepStrictEqual(total([ 1, 1, 1 ]), 3);
-assert.deepStrictEqual(total([ 10, 10, 10 ]), 30);
-assert.deepStrictEqual(total([ 24, -10, 10, 0, 0, 100 ]), 124);
+assert.deepStrictEqual(total([1, 1, 1]), 3);
+assert.deepStrictEqual(total([10, 10, 10]), 30);
+assert.deepStrictEqual(total([24, -10, 10, 0, 0, 100]), 124);
 // End of tests */
