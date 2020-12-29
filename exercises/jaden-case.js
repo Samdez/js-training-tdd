c126de82571fe9c37943c87f9bcb060c5b313c9b
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-29 21:36:59 +0100

    solved jadencase

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -11,9 +11,23 @@
  */
 
 // Your code:
+const jadenCase = (str) => {
+  return str
+    .split(' ')
+    .map(word => {
+      return word
+        .split('')
+        .map((letter, i) => i === 0 ? letter.toUpperCase() : letter.toLowerCase())
+        .join('')
+    })
+    .join(' ')
+  }
 
 //* Begin of tests
 const assert = require('assert');
-
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof jadenCase, 'function')
+assert.strictEqual(jadenCase.length, 1)
+assert.strictEqual(jadenCase('hello world'), 'Hello World')
+assert.strictEqual(jadenCase('hello'), 'Hello')
+// assert.fail('You must write your own tests');
 // End of tests */
