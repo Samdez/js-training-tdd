a09d1d69c85b4b8a905a3aab7da19a42bcfb88e4
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-29 11:52:16 +0100

    solved keep

--- exercises/keep.js
+++ exercises/keep.js
@@ -13,9 +13,28 @@
  */
 
 // Your code:
+const keepFirst = (str) => str.length >= 2 ? str.slice(0, 2) : ''
+const keepLast = (str) => str.length >= 2 ? str.slice(str.length - 2, str.length) : ''
+const keepFirstLast = (str) => str.length >= 2 ? str.slice(2, 4) : str
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof keepFirst, 'function')
+assert.strictEqual(keepFirst.length, 1)
+assert.strictEqual(keepFirst('hello'), 'he')
+assert.strictEqual(keepFirst('h'), '')
+assert.strictEqual(keepFirst(' '), '')
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof keepLast, 'function')
+assert.strictEqual(keepLast.length, 1)
+assert.strictEqual(keepLast('hello'), 'lo')
+assert.strictEqual(keepLast('h'), '')
+assert.strictEqual(keepLast(' '), '')
+
+assert.strictEqual(typeof keepFirstLast, 'function')
+assert.strictEqual(keepFirstLast.length, 1)
+assert.strictEqual(keepFirstLast('hello'), 'll')
+assert.strictEqual(keepFirstLast('h'), 'h')
+assert.strictEqual(keepFirstLast(' '), ' ')
+// assert.fail('You must write your own tests');
 // End of tests */
