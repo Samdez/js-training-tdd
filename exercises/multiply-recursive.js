96e60652ce158fb69454cff28e7332e478122c83
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-12-29 22:21:18 +0100

    solved multiply recursive

--- exercises/multiply-recursive.js
+++ exercises/multiply-recursive.js
@@ -10,6 +10,18 @@
  */
 
 // Your code:
+const multiply = (a, b) => {
+  if (a === 0 || b === 0){
+    return 0
+  }
+  if (a < 0 && b < 0){
+    a = Math.abs(a)
+    b = Math.abs(b)
+  }
+  let min = Math.min(a, b)
+  let max = Math.max(a, b)
+  return max === 1 ? min : min + (multiply(min, max - 1))
+}
 
 //* Begin of tests
 const assert = require('assert');
@@ -21,6 +33,7 @@ assert.strictEqual(multiply.toString().includes('while'), false);
 assert.strictEqual(multiply.toString().includes('for'), false);
 assert.strictEqual(multiply.toString().includes('*'), false);
 assert.strictEqual(multiply.toString().includes('/'), false);
+assert.strictEqual(multiply(2, 4), 8);
 assert.strictEqual(multiply(34, 78), 2652);
 assert.strictEqual(multiply(123, 0), 0);
 assert.strictEqual(multiply(0, -230), 0);
